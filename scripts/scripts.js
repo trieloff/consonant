dad1984748551ec785b88f1c2e7ff179d1b8da13
thi64146 <thi64146@adobe.com>
2022-05-05 12:19:16 -0600

    isColorDark maybe?

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -98,6 +98,32 @@ export async function fetchColorLibrary() {
 }
 
 /**
+ * fetches the color lightness.
+ * @returns {boolean}
+ */
+export function isColorDark(color) {
+  let r, g, b;
+  // Check the format of the color, HEX or RGB?
+  if (color.match(/^rgb/)) {
+    // If HEX --> store the red, green, blue values in separate variables
+    color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+    r = color[1];
+    g = color[2];
+    b = color[3];
+  } else {
+    // If RGB --> Convert it to HEX
+    color = +("0x" + color.slice(1).replace( color.length < 5 && /./g, '$&$&' ));
+    r = color >> 16;
+    g = color >> 8 & 255;
+    b = color & 255;
+  }
+  // HSP equation from http://alienryderflex.com/hsp.html
+  const hsp = Math.sqrt( 0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b) );
+  // Using the HSP value, determine whether the color is light or dark
+  return (hsp < 127.5);
+}
+
+/**
  * Retrieves the content of a metadata tag.
  * @param {string} name The metadata name (or property)
  * @returns {string} The metadata value
