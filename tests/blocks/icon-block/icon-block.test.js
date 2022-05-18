9d007323b5d09f6ee766d4d53c5c7bdffd6c571c
Sartxi <sean.archibeque@gmail.com>
2022-05-18 16:53:37 -0600

    dialed testing

--- tests/blocks/icon-block/icon-block.test.js
+++ tests/blocks/icon-block/icon-block.test.js
@@ -1,33 +1,40 @@
 /* eslint-disable no-unused-expressions */
 /* global describe it */
 
+import init from '../../../blocks/marquee/marquee.js';
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
-import init from '../../../blocks/marquee/marquee.js';
 import { cleanVariations } from '../../../scripts/scripts.js';
 
 const mock = await readFile({ path: './icon-block.mock.html' });
 document.body.innerHTML = mock;
 cleanVariations(document.body);
 
-describe('icon-block', () => {
-    const iconBlocks = document.querySelectorAll('.icon-block');
-    iconBlocks.forEach(icon => init(icon));
+describe('icon blocks', () => {
+    const blocks = document.querySelectorAll('.icon-block');
+    blocks.forEach(block => {
+        init(block);
+        describe('icon block', () => {
+            const children = document.querySelectorAll('.text');
+            if (children.length) {
+                children.forEach(blk => {
+                    it('has an icon', () => {
+                        const icon = blk.querySelector('.detail-M');
+                        expect(icon).to.exist;
+                    });
 
-    describe('default marquee', () => {
-        it('has a heading', () => {
-            const heading = marquees[0].querySelector('.heading-XL');
-            expect(heading).to.exist;
-        });
+                    it('has a heading', () => {
+                        const heading = blk.querySelector('.heading-XL');
+                        expect(heading).to.exist;
+                    });
 
-        it('has an icon', () => {
-            const icon = marquees[0].querySelector('.icon-img');
-            expect(icon).to.exist;
-        });
+                    it('has body text', () => {
+                        const body = blk.querySelector('.body-M');
+                        expect(body).to.exist;
+                    });
+                })
+            }
 
-        it('has body text', () => {
-            const body = marquees[0].querySelector('.body-M');
-            expect(body).to.exist;
-        });
-    });
-});
+        })
+    })
+})
