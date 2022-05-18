48c582da0680422954f87d981e35da05a3a9327f
Sartxi <sean.archibeque@gmail.com>
2022-05-18 16:12:09 -0600

    wrap up tests and use size heights for all types

--- tests/blocks/aside/aside.test.js
+++ tests/blocks/aside/aside.test.js
@@ -1,33 +1,50 @@
 /* eslint-disable no-unused-expressions */
 /* global describe it */
 
+import init from '../../../blocks/aside/aside.js';
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
-import init from '../../../blocks/aside/aside.js';
 import { cleanVariations } from '../../../scripts/scripts.js';
 
 const mock = await readFile({ path: './aside.mock.html' });
 document.body.innerHTML = mock;
 cleanVariations(document.body);
 
+const types = ["simple", "split"];
+
 describe('aside', () => {
     const asides = document.querySelectorAll('.aside');
-    asides.forEach((aside) => init(aside));
+    asides.forEach((aside) => {
+        init(aside);
 
-    describe('default aside', () => {
-        it('has a detail', () => {
-            const detail = asides[0].querySelector('.detail-M');
-            expect(detail).to.exist;
-        });
+        const typeIndex = types.findIndex(v => aside.classList.contains(v));
+        const type = typeIndex >= 0 ? types[typeIndex] : "default";
 
-        it('has a heading', () => {
-            const heading = asides[0].querySelector('.heading-XL');
-            expect(heading).to.exist;
-        });
+        describe(`aside ${type}`, () => {
+            it('has a heading', () => {
+                const heading = aside.querySelector('.heading-XL');
+                expect(heading).to.exist;
+            })
 
-        it('has a body', () => {
-            const body = asides[0].querySelector('.body-S');
-            expect(body).to.exist;
+            it('has a body', () => {
+                const body = aside.querySelector('.body-S');
+                expect(body).to.exist;
+            })
+
+            if (type === "default") {
+                it('has an image', () => {
+                    const image = aside.querySelector('.image');
+                    expect(image).to.exist;
+                })
+            }
+
+            if (type === types[1]) {
+                it('has a background image', () => {
+                    const body = aside.querySelector('.background');
+                    expect(body).to.exist;
+                })
+            }
+
         });
     });
 });
